189839a2b5a089ef023cc3be31d486c3b65fead7
Pradeep <pradeep.varghese@gmail.com>
2018-07-05 12:36:01 +0530

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder array = new StringBuilder(Console.ReadLine());
+            int[] nos = flattenArray(array);
+
+            Console.WriteLine(nos);            
+            Console.ReadLine();
+        }
+
+        private static int[] flattenArray(StringBuilder array)
+        {
+            for (int i = 0; i < array.Length ; i++)
+            {
+                if ((array.ToString().ToArray()[i].Equals('[') || (array.ToString().ToArray()[i].Equals(']'))))
+                {
+                    array.Remove(i, 1);
+                    i--;
+                }
+            }
+            string[] nos = array.ToString().Split(',');
+
+            return  Array.ConvertAll<string, int>(nos, int.Parse);
+        }
+    }
+}
